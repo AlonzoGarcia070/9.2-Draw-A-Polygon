9cb754f98c3d64fae745492638b3267d4326e753
AlonzoGarcia070 <alonzoagarcia73@gmail.com>
2026-04-15 13:27:21 -0500

    not done

--- drawapolygon.py
+++ drawapolygon.py
@@ -5,54 +5,47 @@ def regular(turtle, sides):
         turtle.forward(100)
         turtle.right(360/sides)
 
-
-
-
-
 screen=Screen()
 screen.bgcolor("white")
 screen.title("shapes")
 screen.setup(width=1000,height=500)
 
+polygons = { 3: "triangle", 5 :"pentagon", 6:"hexagon", 7:"septagon", 8: "octogon", 9: "nonogon", 10: "decagon"}
+
 merlaut=Turtle()
 merlaut.color("green")
 
-
 while True:
-    side=int(input("Think of a shape and tell me how many sides does it have: "))
+    sides=int(input("Think of a shape and tell me how many sides does it have: "))
     merlaut.clear()
-    if side<3:
+    if sides<3:
         print("This isn't a shape")
-    elif side ==3:
-        print("this shape is a triangle")
-    elif side==4:
+    elif sides !=4:
+        regular(merlaut, sides)
+        merlaut.write(polygons[sides])
+    elif sides==4:
         print("this shape is a Quadrilateral")
         llel=int(input("How many parallel sides does the shape have: "))
         if llel==0:
                 print("This is a unknown parallel")
         elif llel == 1:
-            print("trapezoid")
+            print("This is a trapezoid")
+        elif llel==4:
+                print("This is a square")
         else:
             llel2=(input("Are all the angles the same measure: "))
             if llel2=="no":
                 print("This is a rhombus")
             elif llel2=="yes":
                 print("This is a rectangle")
-                turtle.forward(100)
-                turtle.right(50)
-                turtle.right(100)
-                turtle.right(50)
-            if llel==4:
-                print("This is a square")
-    elif side==5:
-        print("this shape is a Pentagon")
-        regular(turtle, sides)
-    elif side==6:
-        print("this shape is a hexagon")
-        regular(turtle, sides)
-    else:
-        print("This shape is unknown")
-    regular(merlaut, side)
+                # merlaut.begin_fill()
+                for i in range(2):
+                    merlaut.forward(100)
+                    merlaut.right(90)
+                    merlaut.forward(50)
+                    merlaut.right(90)
+                # merlaut.end_fill()
+
 
     
 
@@ -70,36 +63,3 @@ while True:
 #     print("this shape is a hexagon")
 # else:
 #     print("This shape is unknown")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
